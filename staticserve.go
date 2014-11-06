32400098bc41aabf6c851342f0693cf4eee31d0d
Ian Bishop <ianbishop@pace7.com>
2014-11-06 15:48:45 +1000

    add stripPrefix flag

--- staticserve.go
+++ staticserve.go
@@ -5,13 +5,24 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 )
 
 func main() {
 	webroot := flag.String("webroot", "/usr/share/doc", "path to web files")
+	stripPrefix := flag.String("stripPrefix", "", "path to strip from incoming requests")
 	port := flag.Int("port", 8080, "port to listen on")
 	flag.Parse()
 
 	log.Printf("Listening on port %d\n", *port)
-	panic(http.ListenAndServe(":"+strconv.Itoa(*port), http.FileServer(http.Dir(*webroot))))
+
+	if *stripPrefix != "" {
+		log.Printf("stripPrefix '%s'\n", *stripPrefix)
+		// Handle either case of trailing slash
+		stripPrefixTrim := strings.TrimRight(*stripPrefix, "/")
+		http.Handle(stripPrefixTrim+"/", http.StripPrefix(stripPrefixTrim+"/", http.FileServer(http.Dir(*webroot))))
+	} else {
+		http.Handle("/", http.FileServer(http.Dir(*webroot)))
+	}
+	panic(http.ListenAndServe(":"+strconv.Itoa(*port), nil))
 }
